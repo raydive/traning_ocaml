5645ddc667a1f6081e90bb2df258d5adfb16c196
raydive <274594+raydive@users.noreply.github.com>
2026-05-16 18:14:37 +0900

    Add sexplib

--- hello/_build/default/bin/main.ml
+++ hello/_build/default/bin/main.ml
@@ -1,1 +1,6 @@
 let () = Printf.printf "%s\n" Hello.En.v
+let () = Printf.printf "%s\n" Hello.Es.v
+
+let exp1 = Sexplib.Sexp.of_string "(This (is an) (s expression))"
+
+let () = Printf.printf "%s\n" (Sexplib.Sexp.to_string exp1)
